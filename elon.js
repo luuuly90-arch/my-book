b95c5b0724f5717a115aa32aa49289ec7d17b5af
luuuly90-arch <luuuly90@gmail.com>
2026-03-18 09:31:00 +0300

    Add files via upload

---
+++ elon.js
@@ -0,0 +1,31 @@
+<div class="items">
+
+    <div class="item-card" onclick="buy(85000, 'car')">
+        <span class="icon">🚗</span>
+        <h3>سيارة</h3>
+        <p>85,000 ريال</p>
+        <p>عدد السيارات: <span id="carCount">0</span></p>
+    </div>
+
+    <div class="item-card" onclick="buy(12000000, 'plane')">
+        <span class="icon">✈️</span>
+        <h3>طيارة</h3>
+        <p>12,000,000 ريال</p>
+        <p>عدد الطيارات: <span id="planeCount">0</span></p>
+    </div>
+
+    <div class="item-card" onclick="buy(950000, 'house')">
+        <span class="icon">🏠</span>
+        <h3>بيت</h3>
+        <p>950,000 ريال</p>
+        <p>عدد البيوت: <span id="houseCount">0</span></p>
+    </div>
+
+    <div class="item-card" onclick="buy(2500, 'phone')">
+        <span class="icon">📱</span>
+        <h3>جوال</h3>
+        <p>2,500 ريال</p>
+        <p>عدد الجوالات: <span id="phoneCount">0</span></p>
+    </div>
+
+</div>
